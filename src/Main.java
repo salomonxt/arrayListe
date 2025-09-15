fe334ca0fdaa0ae47fdcbfaac9740e87236389b1
salomonxt <salomonxt@protonmail.ch>
2025-09-15 17:30:21 +0200

    add Liste erstellt und Mirrrraaa aufgenommen

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,21 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         String[] names = {"Adam", "Bernd", "Cäsar"};
 
         System.out.println(Arrays.toString(names));
+
+        //neu // String, weil Strings rein sollen
+        List<String> namesListe = new ArrayList<>();
+        namesListe.add("Mirrra");
+        System.out.println("namesListe = " + namesListe);
+        System.out.println(namesListe.getLast());
+
     }
+
 
 
 }
